6fbb8c7d2698410517e8be1e343ff99bddad2c6f
Fandy <fandymsimanjorang@gmail.com>
2026-02-28 09:46:26 +0700

    Penggunaan operator

---
+++ latihanPpanjang.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float luas, p, l;
+
+    cout << "Masukkan panjang : ";
+    cin >> p;
+
+    cout << "Masukkan lebar : ";
+    cin >> l;
+
+    luas = p * l;
+    cout << "Luas = " << luas << endl;
+}
